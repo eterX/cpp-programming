7b9b03b9bd6b5bc1567f3fb947e9a2198c535088
Vex Woo <hap.ddup@gmail.com>
2016-07-17 04:27:25 -0500

    add union data structure

--- basic/union.cpp
+++ basic/union.cpp
@@ -19,7 +19,7 @@ int main(int argc, const char* argv[]) {
     version_t ver;
     ver.version = 0x00020001;
     printf("Version: %08x\n", ver.version);
-    printf("Min Ver: %02x\n", ver.min_ver);
-    printf("Max Ver: %02x\n", ver.max_ver);
+    printf("Min Ver: %04x\n", ver.min_ver);
+    printf("Max Ver: %04x\n", ver.max_ver);
     return 0;
 }
