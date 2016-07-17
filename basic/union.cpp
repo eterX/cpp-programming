8dc913b47181fd2ebc7a1998d34ded5763fbbb3b
Vex Woo <hap.ddup@gmail.com>
2016-07-17 04:26:27 -0500

    add union data structure

---
+++ basic/union.cpp
@@ -0,0 +1,25 @@
+// Copyright[2016] <Vex Woo>
+
+#include <iostream>
+
+/*
+  Unions allow one portion of memory to be accessed as different data types.
+  Its declaration and use is similar to the one of structures,
+*/
+
+int main(int argc, const char* argv[]) {
+    union version_t {
+        int version;
+        struct {
+          short min_ver;
+          short max_ver;
+        };
+    };
+
+    version_t ver;
+    ver.version = 0x00020001;
+    printf("Version: %08x\n", ver.version);
+    printf("Min Ver: %02x\n", ver.min_ver);
+    printf("Max Ver: %02x\n", ver.max_ver);
+    return 0;
+}
